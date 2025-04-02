d08683faaa6eba50bde8483547babcbb151dd813
gbgo <gbgo@cin.ufpe.br>
2025-04-02 19:44:33 -0300

    add data integration with backend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
 import './App.css';
 import InputForm from './components/InputForm';
 import ResultsDisplay from './components/ResultsDisplay';
-import { useState } from 'react';
-import { calculateRIM } from './utils/rimMethod';
+import { useState, useEffect } from 'react';
 
 function App() {
   const [results, setResults] = useState(null);
+  const [pyodide, setPyodide] = useState(null);
+  const [loadingPyodide, setLoadingPyodide] = useState(true);
 
-  const handleFormSubmit = (data) => {
-    const calculationResults = calculateRIM(data);
-    setResults(calculationResults);
+  useEffect(() => {
+    async function loadPyodideAndPackages() {
+      try {
+        const pyodideInstance = await window.loadPyodide({
+          packages: ["numpy"]
+        });
+    
+        await pyodideInstance.runPythonAsync(`
+          import micropip
+          await micropip.install("sad-cin")
+          from sad_cin import decision_support
+          import json
+          
+          def get_results(json_str):
+              data = json.loads(json_str)
+              result = decision_support(data)
+              return json.dumps(result)
+        `);
+        setPyodide(pyodideInstance);
+      } catch (error) {
+        console.error("Erro ao carregar o Pyodide:", error);
+      } finally {
+        setLoadingPyodide(false);
+      }
+    }
+    loadPyodideAndPackages();
+  }, []);
+
+
+  const handleFormSubmit = (jsonStr) => {
+    if (!pyodide) {
+      console.error("Pyodide ainda não está carregado");
+      return;
+    }
+    try {
+      console.log("Dados recebidos:", jsonStr);
+      const getResults = pyodide.globals.get("get_results");
+      const resultString = getResults(jsonStr);
+      setResults(JSON.parse(resultString));
+    } catch (error) {
+      console.error("Error running Python function:", error);
+    }
   };
-
   return (
     <div className="App">
       <header className="App-header">
         <h1>Método RIM - MCDM Profissional</h1>
       </header>
       <main className="container">
-        <InputForm onSubmit={handleFormSubmit} />
+      <InputForm onSubmit={handleFormSubmit} disabled={loadingPyodide} />
         {results && <ResultsDisplay results={results} />}
       </main>
     </div>
